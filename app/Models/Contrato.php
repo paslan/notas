b410e73fb49e16484a597158f1009fb9318002ac
Paulo Aslan <paslan@gmail.com>
2023-02-23 10:37:23 -0300

    Ajuste Cidade Estado - Empresa

--- app/Models/Contrato.php
+++ app/Models/Contrato.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Contrato extends Model
 {
     use HasFactory;
+
+    public function contratos()
+    {
+        return $this->hasMany(Contrato::class);
+    }
 }
